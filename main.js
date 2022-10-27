71a9d4dc2a51611496e0df48fb79182f7726f620
demme13 <116792408+demme13@users.noreply.github.com>
2022-10-27 09:36:29 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+document.querySelector(".button").addEventListener("click", function () {
+    let b= document.querySelector("#Youtube");
+    b.classList.remove("rotate");
+    setTimeout(() => {
+        b.classList.add("rotate");
+    }, 0);    
+  });
+  document.querySelector(".button").addEventListener("click", function () {
+    let b= document.querySelector("#Youtube");
+    b.classList.remove("rotate");
+    setTimeout(() => {
+        b.classList.add("rotate");
+    }, 0);    
+  });
+  document.querySelector(".button").addEventListener("click", function () {
+    let b= document.querySelector("#Instagram");
+    b.classList.remove("rotate");
+    setTimeout(() => {
+        b.classList.add("rotate");
+    }, 0);    
+  });
